721a5ce25ecb514c3e9f1797a88d484d9d4e4ca3
Fabian Greif <fabian.greif@rwth-aachen.de>
2016-10-15 14:48:21 +0200

    Improve graphviz output by indenting and sorting name.

--- lbuild/builder/dependency.py
+++ lbuild/builder/dependency.py
@@ -13,12 +13,13 @@ import re
 import lbuild.parser
 
 def get_valid_identifier(name):
-    return re.sub('\W|^(?=\d)', '_', name)
+    return re.sub(r'\W|^(?=\d)', '_', name)
 
 def graphviz(repositories, clustered=False):
     output = []
-    output.append("digraph dependencies {")
-    # output.append("rankdir=LR;")
+    output.append("digraph dependencies")
+    output.append("{")
+    # output.append("\trankdir=LR;")
 
     modules = {}
     for repository in repositories.values():
@@ -27,26 +28,29 @@ def graphviz(repositories, clustered=False):
 
     for name, repository in repositories.items():
         if clustered:
-            output.append("subgraph cluster{} {{".format(get_valid_identifier(name)))
+            output.append("\tsubgraph cluster{}".format(get_valid_identifier(name)))
         else:
-            output.append("subgraph {} {{".format(get_valid_identifier(name)))
-        output.append("label = \"{}\";".format(name))
-        output.append("node [style=filled, shape=box];")
-        for module in repository.modules.values():
+            output.append("\tsubgraph {}".format(get_valid_identifier(name)))
+        output.append("\t{")
+        output.append("\t\tlabel = \"{}\";".format(name))
+        output.append("\t\tnode [style=filled, shape=box];")
+        output.append("")
+        for module in sorted(repository.modules.values()):
             if clustered:
-                # Remove the repository name
+                # Remove the repository name for the clusted output. The
+                # repository name is meantioned in the cluster already.
                 name = ":\\n".join(module.fullname.split(":")[1:])
             else:
                 name = ":\\n".join(module.fullname.split(":"))
-            output.append("{} [label=\"{}\"];".format(get_valid_identifier(module.fullname), name))
+            output.append("\t\t{} [label=\"{}\"];".format(get_valid_identifier(module.fullname), name))
 
-        output.append("}")
+        output.append("\t}")
 
-    for module in modules.values():
-        for dep in module.dependencies:
+    for module in sorted(modules.values()):
+        for dep in sorted(module.dependencies):
             dep = lbuild.parser.Parser.find_module(modules, dep)
-            output.append("{} -> {};".format(get_valid_identifier(module.fullname),
-                                             get_valid_identifier(dep.fullname)))
+            output.append("\t{} -> {};".format(get_valid_identifier(module.fullname),
+                                               get_valid_identifier(dep.fullname)))
 
     output.append("}")
     return "\n".join(output)
