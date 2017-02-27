178247dc3193f221824636e62cf9a32c1ffc3dfd
Fabian Greif <fabian.greif@rwth-aachen.de>
2017-02-27 23:34:20 +0100

    Cleanup main script.
    Splits the command line argument parser into several smaller actions.

--- lbuild/__main__.py
+++ lbuild/__main__.py
@@ -15,6 +15,7 @@ import traceback
 
 import lbuild.parser
 import lbuild.logger
+import lbuild.module
 import lbuild.vcs.common
 
 
@@ -38,6 +39,212 @@ def is_repository_option(option_name):
         return True
     else:
         return False
+
+
+class InitAction:
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("init",
+            help="Load remote repositories into the cache folder.")
+        parser.set_defaults(execute_action=self.perform)
+
+    def perform(self, args, config):
+        lbuild.vcs.common.initialize(config)
+
+
+class UpdateAction:
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("update",
+            help="Update the content of remote repositories in the cache folder.")
+        parser.set_defaults(execute_action=self.perform)
+
+    def perform(self, args, config):
+        lbuild.vcs.common.update(config)
+
+
+class ManipulationActionBase:
+    """
+    Base class for actions that interact directly with the parser repositories.
+    """
+    def prepare_repositories(self, args, config):
+        parser = lbuild.parser.Parser()
+        parser.load_repositories(config, args.repositories)
+
+        commandline_options = config.format_commandline_options(args.options)
+        repo_options = parser.merge_repository_options(config.options, commandline_options)
+
+        self.perform(args, parser, config, repo_options)
+
+
+class DiscoverRepositoryAction(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("discover-repository",
+            aliases=['repo'],
+            help="Display the repository options of all selected repositories.")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        for option in sorted(list(repo_options.values())):
+            print(option.format())
+
+
+class DiscoverModulesActions(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("discover-modules",
+            aliases=['modules'],
+            help="Inspect all available modules with the given repository options. "
+                 "All repository options must be defined.")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        modules = parser.prepare_repositories(repo_options)
+        for module in sorted(list(modules.values())):
+            print(module)
+
+
+class DependenciesAction(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("dependencies",
+            help="Generate a grahpviz representation of the module dependencies.")
+        parser.add_argument("-m", "--module",
+            dest="modules",
+            type=str,
+            action="append",
+            default=[],
+            help="Select specific modules.")
+        parser.add_argument("-n", "--depth",
+            dest="depth",
+            type=int,
+            default=sys.maxsize,
+            help="Only show dependencies up to a specific depth. Only valid if "
+                 "specific modules are selected, otherwise all modules are printed "
+                 "anyways.")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        available_modules = parser.prepare_repositories(repo_options)
+
+        if len(args.modules) == 0:
+            selected_modules = [":**"]
+        else:
+            selected_modules = args.modules
+        selected_modules = lbuild.module.resolve_modules(available_modules,
+                                                         selected_modules)
+        dot_file = lbuild.builder.dependency.graphviz(available_modules,
+                                                      selected_modules,
+                                                      args.depth,
+                                                      clustered=False)
+        print(dot_file)
+
+
+class DiscoverModuleOptionsAction(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("discover-module-options",
+            aliases=['options'],
+            help="Inspect the module options of one or more modules (if specified "
+                 "through the module option(s)) or all available modules (if no "
+                 "module is specified).")
+        parser.add_argument("-m", "--module",
+            dest="modules",
+            type=str,
+            action="append",
+            default=[],
+            help="Select a specific module.")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        if len(args.modules) == 0 and len(config.selected_modules) == 0:
+            config.selected_modules.extend([":**"])
+        else:
+            config.selected_modules.extend(args.modules)
+        _, options = get_modules(parser, repo_options, config.options, config.selected_modules)
+
+        for option in sorted(list(options.values())):
+            print(option.format())
+
+            if option.short_description:
+                print()
+                print(textwrap.indent(option.short_description, "  "))
+                print()
+
+
+class DiscoverOptionAction(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("discover-option",
+            aliases=['option'],
+            help="Print the description and values of one option.")
+        parser.add_argument("-o", "--option-name",
+            dest="option_name",
+            required=True,
+            help="Select a specific module")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        option_name = args.option_name
+        if is_repository_option(option_name):
+            option = lbuild.module.find_module(repo_options, option_name)
+        else:
+            _, options = get_modules(parser, repo_options, config.options)
+            option = lbuild.module.find_module(options, option_name)
+
+        print(option.factsheet())
+        print()
+
+
+class DiscoverOptionValuesAction(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("discover-option-values",
+            aliases=['option-values'],
+            help="Print the values of one option.")
+        parser.add_argument("-o", "--option-name",
+            dest="option_name",
+            required=True,
+            help="Select a specific module")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        option_name = args.option_name
+        if is_repository_option(option_name):
+            option = lbuild.module.find_module(repo_options, option_name)
+        else:
+            _, options = get_modules(parser, repo_options, config.options)
+            option = lbuild.module.find_module(options, option_name)
+
+        for value in lbuild.utils.listify(option.values):
+            print(value)
+
+
+class BuildAction(ManipulationActionBase):
+    def register(self, argument_parser):
+        parser = argument_parser.add_parser("build",
+            help="Generate the library source code blob with the given options.")
+        parser.add_argument("-m", "--module",
+            dest="modules",
+            type=str,
+            action="append",
+            default=[],
+            help="Select a specific module.")
+        parser.add_argument("--no-log",
+            dest="buildlog",
+            action="store_false",
+            default=True,
+            help="Do not create a build log. This log contains all files being "
+                 "generated, their source files and the module which generated "
+                 "the file.")
+        parser.set_defaults(execute_action=self.prepare_repositories)
+
+    def perform(self, args, parser, config, repo_options):
+        log = lbuild.buildlog.BuildLog()
+
+        config.selected_modules.extend(args.modules)
+        build_modules, module_options = get_modules(parser, repo_options, config.options, config.selected_modules)
+        parser.build_modules(args.path, build_modules, repo_options, module_options, log)
+
+        if args.buildlog:
+            configfilename = args.config
+            logfilename = configfilename + ".log"
+            with open(logfilename, "wb") as logfile:
+                logfile.write(log.to_xml(to_string=True))
+
 
 def prepare_argument_parser():
     """
@@ -85,173 +292,38 @@ def prepare_argument_parser():
     subparsers = argument_parser.add_subparsers(title="Actions",
         dest="action")
 
-    parser_init = subparsers.add_parser("init",
-        help="Load remote repositories into the cache folder.")
-    parser_update = subparsers.add_parser("update",
-        help="Update the content of remote repositories in the cache folder.")
-    parser_discover_repo = subparsers.add_parser("discover-repository",
-        aliases=['repo'],
-        help="Display the repository options of all selected repositories.")
-    parser_discover_modules = subparsers.add_parser("discover-modules",
-        aliases=['modules'],
-        help="Inspect all available modules with the given repository options. "
-             "All repository options must be defined.")
-    parser_dependecies = subparsers.add_parser("dependencies",
-        help="Generate a grahpviz representation of the module dependencies.")
-    parser_dependecies.add_argument("-m", "--module",
-        dest="modules",
-        type=str,
-        action="append",
-        default=[],
-        help="Select specific modules.")
-    parser_dependecies.add_argument("-n", "--depth",
-        dest="depth",
-        type=int,
-        default=sys.maxsize,
-        help="Only show dependencies up to a specific depth. Only valid if "
-             "specific modules are selected, otherwise all modules are printed "
-             "anyways.")
-
-    parser_discover_module_options = subparsers.add_parser("discover-module-options",
-        aliases=['options'],
-        help="Inspect the module options of one or more modules (if specified "
-             "through the module option(s)) or all available modules (if no "
-             "module is specified).")
-    parser_discover_module_options.add_argument("-m", "--module",
-        dest="modules",
-        type=str,
-        action="append",
-        default=[],
-        help="Select a specific module.")
-
-    parser_discover_option = subparsers.add_parser("discover-option",
-        aliases=['option'],
-        help="Print the description and values of one option.")
-    parser_discover_option.add_argument("-o", "--option-name",
-        dest="option_name",
-        required=True,
-        help="Select a specific module")
-
-    parser_discover_option_values = subparsers.add_parser("discover-option-values",
-        aliases=['option-values'],
-        help="Print the values of one option.")
-    parser_discover_option_values.add_argument("-o", "--option-name",
-        dest="option_name",
-        required=True,
-        help="Select a specific module")
-
-    parser_build = subparsers.add_parser("build",
-        help="Generate the library source code blob with the given options.")
-    parser_build.add_argument("-m", "--module",
-        dest="modules",
-        type=str,
-        action="append",
-        default=[],
-        help="Select a specific module.")
-    parser_build.add_argument("--no-log",
-        dest="buildlog",
-        action="store_false",
-        default=True,
-        help="Do not create a build log. This log contains all files being "
-             "generated, their source files and the module which generated "
-             "the file.")
+    actions = [
+        InitAction(),
+        UpdateAction(),
+        DiscoverRepositoryAction(),
+        DiscoverModulesActions(),
+        DependenciesAction(),
+        DiscoverModuleOptionsAction(),
+        DiscoverOptionAction(),
+        DiscoverOptionValuesAction(),
+        BuildAction(),
+    ]
+    for action in actions:
+        action.register(subparsers)
 
     return argument_parser
 
 def run(args):
+    lbuild.logger.configure_logger(args.verbose)
+
     config = lbuild.config.Configuration.parse_configuration(args.config)
-
-    if args.action == 'init':
-        lbuild.vcs.common.initialize(config)
-    elif args.action == 'update':
-        lbuild.vcs.common.update(config)
-    else:
-        parser = lbuild.parser.Parser()
-        parser.load_repositories(config, args.repositories)
-
-        commandline_options = config.format_commandline_options(args.options)
-        repo_options = parser.merge_repository_options(config.options, commandline_options)
-
-        if args.action in ['discover-repository', 'repo']:
-            for option in sorted(list(repo_options.values())):
-                print(option.format())
-        elif args.action in ['discover-modules', 'modules']:
-            modules = parser.prepare_repositories(repo_options)
-            for module in sorted(list(modules.values())):
-                print(module)
-        elif args.action in ['dependencies']:
-            available_modules = parser.prepare_repositories(repo_options)
-
-            if len(args.modules) == 0:
-                selected_modules = [":**"]
-            else:
-                selected_modules = args.modules
-            selected_modules = lbuild.module.resolve_modules(available_modules,
-                                                             selected_modules)
-            dot_file = lbuild.builder.dependency.graphviz(available_modules,
-                                                          selected_modules,
-                                                          args.depth,
-                                                          clustered=False)
-            print(dot_file)
-        elif args.action in ['discover-module-options', 'options']:
-            if len(args.modules) == 0 and len(config.selected_modules) == 0:
-                config.selected_modules.extend([":**"])
-            else:
-                config.selected_modules.extend(args.modules)
-            _, options = get_modules(parser, repo_options, config.options, config.selected_modules)
-
-            for option in sorted(list(options.values())):
-                print(option.format())
-
-                if option.short_description:
-                    print()
-                    print(textwrap.indent(option.short_description, "  "))
-                    print()
-        elif args.action in ['discover-option', 'option']:
-            option_name = args.option_name
-            if is_repository_option(option_name):
-                option = parser.find_module(repo_options, option_name)
-            else:
-                _, options = get_modules(parser, repo_options, config.options)
-                option = parser.find_module(options, option_name)
-
-            print(option.factsheet())
-            print()
-        elif args.action in ['discover-option-values', 'option-values']:
-            option_name = args.option_name
-            if is_repository_option(option_name):
-                option = parser.find_module(repo_options, option_name)
-            else:
-                _, options = get_modules(parser, repo_options, config.options)
-                option = parser.find_module(options, option_name)
-
-            for value in lbuild.utils.listify(option.values):
-                print(value)
-        elif args.action == 'build':
-            log = lbuild.buildlog.BuildLog()
-
-            config.selected_modules.extend(args.modules)
-            build_modules, module_options = get_modules(parser, repo_options, config.options, config.selected_modules)
-            parser.build_modules(args.path, build_modules, repo_options, module_options, log)
-
-            if args.buildlog:
-                configfilename = args.config
-                logfilename = configfilename + ".log"
-                with open(logfilename, "wb") as logfile:
-                    logfile.write(log.to_xml(to_string=True))
-        else:
-            raise lbuild.exception.BlobArgumentException("Unknown command-line "
-                                                         "option '{}'".format(args.action))
+    args.execute_action(args, config)
 
 def main():
     """
     Main entry point of lbuild.
     """
-    argument_parser = prepare_argument_parser()
-    args = argument_parser.parse_args(sys.argv[1:])
-
-    lbuild.logger.configure_logger(args.verbose)
     try:
+        argument_parser = prepare_argument_parser()
+
+        commandline_arguments = sys.argv[1:]
+        args = argument_parser.parse_args(commandline_arguments)
+
         run(args)
     except lbuild.exception.BlobArgumentException as error:
         argument_parser.print_help()
